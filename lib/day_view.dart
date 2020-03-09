96a91413e3b00a58b3b5d74a672d1a5ddff6d61c
Fabien MARIE-LOUISE <fabien@MacBook-Pro-de-Fabien.local>
2020-03-09 23:28:33 +0100

    save

--- lib/day_view.dart
+++ lib/day_view.dart
@@ -7,7 +7,7 @@ class DayView extends StatefulWidget {
   static const int HOURS_PER_DAY = 24;
   static const int MINUTES_PER_DAY = 1440;
   static const double DEFAULT_HOUR_ROW_HEIGHT = 60;
-  static const double DEFAULT_MIN_EVENT_HEIGHT = DEFAULT_HOUR_ROW_HEIGHT / 6;
+  static const double DEFAULT_MIN_EVENT_HEIGHT = DEFAULT_HOUR_ROW_HEIGHT / 4;
   static const double DEFAULT_EVENT_ROW_LEFT_OFFSET = 60;
   double hourRowHeight;
 
@@ -62,6 +62,23 @@ class _DayViewState extends State<DayView> {
               children: _buildHourRows(today),
             ),
           ),
+          ...List<Widget>.generate(97, (index) {
+            return Positioned(
+              top: (index * DayView.DEFAULT_MIN_EVENT_HEIGHT)+ 8,
+              left:  DayView.DEFAULT_EVENT_ROW_LEFT_OFFSET,
+              width: availableEventWidth,
+              height: DayView.DEFAULT_MIN_EVENT_HEIGHT,
+              child: Container(
+                child: DragTarget<Event>(
+                  builder: (context, candidates, rejects) {
+                    return candidates.length > 0 ? Container(
+                      color: Colors.green,
+                    ) : null;
+                  },
+                ),
+              ),
+            );
+          }).toList(),
           ..._buildPositionedEvents(availableEventWidth),
           Positioned(
             top: _getTopPositionFromDateTime(today),
@@ -120,18 +137,20 @@ class _DayViewState extends State<DayView> {
     List<Event> events = [
       Event("1", DateTime(now.year, now.month, now.day, 10, 0, 0),
           DateTime(now.year, now.month, now.day, 10, 15, 0), false, "Event 1"),
-      Event("2", DateTime(now.year, now.month, now.day, 14, 42, 0),
-          DateTime(now.year, now.month, now.day, 14, 55, 0), false, "Event 2"),
+      Event("2", DateTime(now.year, now.month, now.day, 14, 45, 0),
+          DateTime(now.year, now.month, now.day, 15, 00, 0), false, "Event 2"),
       Event("3", DateTime(now.year, now.month, now.day, 10, 15, 0),
           DateTime(now.year, now.month, now.day, 10, 30, 0), false, "Event 3"),
       Event("4", DateTime(now.year, now.month, now.day, 10, 30, 0),
           DateTime(now.year, now.month, now.day, 10, 45, 0), false, "Event 4"),
-      Event("5",
+      Event(
+          "5",
           DateTime(now.year, now.month, now.day, 10, 45, 0),
           DateTime(now.year, now.month, now.day, 11, 0, 0),
           false,
           "Event 5 lorem ipsum dolor sit amet"),
-      Event("6",
+      Event(
+          "6",
           DateTime(now.year, now.month, now.day, 8, 0, 0),
           DateTime(now.year, now.month, now.day, 9, 0, 0),
           false,
@@ -146,19 +165,23 @@ class _DayViewState extends State<DayView> {
           DateTime(now.year, now.month, now.day, 9, 0, 0), false, "Event 10"),
       Event("11", DateTime(now.year, now.month, now.day, 8, 32, 0),
           DateTime(now.year, now.month, now.day, 9, 0, 0), false, "Event 11"),
+      Event("12", DateTime(now.year, now.month, now.day, 14, 45, 0),
+          DateTime(now.year, now.month, now.day, 15, 00, 0), false, "Event 12"),
     ];
 
     SplayTreeMap<int, List<Event>> groupedEvents =
         SplayTreeMap((a, b) => a.compareTo(b));
 
     events.forEach((event) {
-      if (groupedEvents.containsKey(event.startTimeInMinutes)) {
-        groupedEvents[event.startTimeInMinutes].add(event);
+      final key = _getKeyForEvent(event);
+
+      if (groupedEvents.containsKey(key)) {
+        groupedEvents[key].add(event);
       } else {
-        groupedEvents[event.startTimeInMinutes] = [event];
+        groupedEvents[key] = [event];
       }
     });
-    
+
     groupedEvents.values.forEach((eventList) {
       final eventWidth = rowWidth / eventList.length;
       int index = 0;
@@ -166,21 +189,13 @@ class _DayViewState extends State<DayView> {
       eventList.sort((a, b) => a.startDate.compareTo(b.startDate));
 
       eventList.forEach((event) {
-
-        final int indent = groupedEvents.values.expand((it) => it.toList()).where((it) {
-          if(it.id == event.id) {
-            return false;
-          } else if(it.startTimeInMinutes == event.startTimeInMinutes) {
-            return false;
-          } else if(it.startDate.isBefore(event.startDate) && it.endDate.isAfter(event.endDate)) {
-            return true;
-          } else if(it.endDate.isAfter(event.startDate) && it.startDate.isBefore(event.startDate)) {
-            return true;
-          }
-          return false;
-        }).length;
-
-        print("${event.id} -> $indent");
+        final int indent = groupedEvents.values
+            .expand((it) => it.toList())
+            .where((it) =>
+                it.startDate.isBefore(event.startDate) &&
+                it.endDate.isAfter(event.startDate))
+            .where((it) => _getKeyForEvent(it) != _getKeyForEvent(event))
+            .length;
 
         final topOffset = _getTopPositionFromDateTime(event.startDate) +
             (HourLineSeparator.DEFAULT_HEIGHT / 2) +
@@ -200,46 +215,41 @@ class _DayViewState extends State<DayView> {
           fontSize = (eventHeight / 2);
         }
 
-        positionedEvents.add(Positioned(
-          left: DayView.DEFAULT_EVENT_ROW_LEFT_OFFSET + (eventWidth * index) + (indent * 5),
-          top: topOffset,
-          height: eventHeight,
+        final eventCell = DraggableEventCell(
+          event: event,
+          isShortEvent: isShortEvent,
+          fontSize: fontSize,
           width: eventWidth,
-          child: Container(
-            color: Colors.blue[100].withOpacity(0.7),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  width: 1,
-                  color: Colors.blue[500],
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: isShortEvent
-                        ? EdgeInsets.symmetric(vertical: 2, horizontal: 4)
-                        : EdgeInsets.all(4),
-                    child: Text(
-                      event.title,
-                      maxLines: isShortEvent ? 1 : 3,
-                      overflow: TextOverflow.ellipsis,
-                      //softWrap: true,
-                      style: TextStyle(
-                        color: Colors.blue[800],
-                        fontSize: fontSize,
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
+          height: eventHeight,
+          textColor: Colors.blue[800],
+          backgroundColor: Colors.blue[100].withOpacity(0.7),
+          textColorInverted: Colors.white,
+          backgroundColorInverted: Colors.blue[500],
+          separatorColor: Colors.blue[500],
+        );
+
+        positionedEvents.add(
+          Positioned(
+            left: DayView.DEFAULT_EVENT_ROW_LEFT_OFFSET +
+                (eventWidth * index) +
+                (indent * 5),
+            top: topOffset,
+            height: eventHeight,
+            width: eventWidth,
+            child: eventCell,
           ),
-        ));
+        );
         index++;
       });
     });
 
     return positionedEvents;
+  }
+
+  int _getKeyForEvent(Event event) {
+    // key is start time floored to nearest quarter hour in minutes
+    // ex: 01:27 -> 01:15 -> 75
+    return event.startDate.floorToNearestQuarterHour().roundToMinutes();
   }
 
   double _getTopPositionFromDateTime(DateTime date) {
@@ -249,6 +259,151 @@ class _DayViewState extends State<DayView> {
 
   double _getEventHeightFromDuration(int durationInMinutes) {
     return (sizeOfOneMinute * durationInMinutes) - 2;
+  }
+}
+
+class DraggableEventCell extends StatelessWidget {
+  final Event event;
+  final bool isShortEvent;
+  final double fontSize;
+  final double width;
+  final double height;
+  final Color textColor;
+  final Color backgroundColor;
+  final Color textColorInverted;
+  final Color backgroundColorInverted;
+  final Color separatorColor;
+
+  DraggableEventCell({
+    @required this.event,
+    @required this.isShortEvent,
+    @required this.fontSize,
+    @required this.width,
+    @required this.height,
+    @required this.textColor,
+    @required this.backgroundColor,
+    @required this.textColorInverted,
+    @required this.backgroundColorInverted,
+    @required this.separatorColor,
+  });
+
+  EventCellContent _buildCellContent(Color textColor, Color backgroundColor) {
+    return EventCellContent(
+      event: event,
+      isShortEvent: isShortEvent,
+      fontSize: fontSize,
+      height: height,
+      width: width,
+      textColor: textColor,
+      backgroundColor: backgroundColor,
+      separatorColor: separatorColor,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final content = _buildCellContent(
+      textColor,
+      backgroundColor,
+    );
+
+    final contentWithColorInverted = _buildCellContent(
+      textColorInverted,
+      backgroundColorInverted,
+    );
+
+    return LongPressDraggable<Event>(
+      data: event,
+      hapticFeedbackOnStart: true,
+      childWhenDragging: Container(
+        height: height,
+        width: width,
+        color: Colors.orange[100],
+      ),
+      feedback: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          boxShadow: [
+            new BoxShadow(
+              color: contentWithColorInverted.backgroundColor.withOpacity(0.5),
+              offset: new Offset(0, 5),
+              blurRadius: 10,
+            )
+          ],
+        ),
+        child: contentWithColorInverted,
+      ),
+      child: content,
+    );
+  }
+}
+
+class EventCellContent extends StatelessWidget {
+  final Event event;
+  final bool isShortEvent;
+  final double fontSize;
+  final double width;
+  final double height;
+  final Color textColor;
+  final Color backgroundColor;
+  final Color separatorColor;
+
+  const EventCellContent({
+    @required this.event,
+    @required this.isShortEvent,
+    @required this.fontSize,
+    @required this.width,
+    @required this.height,
+    @required this.textColor,
+    @required this.backgroundColor,
+    @required this.separatorColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      color: backgroundColor,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 1,
+            color: separatorColor,
+          ),
+          Expanded(
+            child: Padding(
+              padding: isShortEvent
+                  ? EdgeInsets.symmetric(vertical: 2, horizontal: 4)
+                  : EdgeInsets.all(4),
+              child: Text(
+                event.title,
+                maxLines: isShortEvent ? 1 : 3,
+                overflow: TextOverflow.ellipsis,
+                //softWrap: true,
+                style: TextStyle(
+                    color: textColor,
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.normal,
+                    decoration: TextDecoration.none),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+extension DateTimeExtension on DateTime {
+  DateTime floorToNearestQuarterHour() {
+    return this.subtract(Duration(minutes: (this.minute % 15)));
+  }
+
+  int roundToMinutes() {
+    return (this.hour * 60) + this.minute;
   }
 }
 
@@ -263,19 +418,6 @@ class Event {
 
   int get durationInMinutes {
     return startDate.difference(endDate).inMinutes.abs();
-  }
-
-  int get startTimeInMinutes {
-    int min = 0;
-
-    if (startDate.minute < 10) {
-      min = 0;
-    } else {
-      int minToSubstract = (startDate.minute % 10.0).abs().round();
-      min = startDate.subtract(Duration(minutes: minToSubstract)).minute;
-    }
-
-    return (startDate.hour * 60) + min;
   }
 }
 
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterdayview/DayView.dart';
+import 'package:flutterdayview/day_view.dart';
 
 class HomePage extends StatelessWidget {
   @override
